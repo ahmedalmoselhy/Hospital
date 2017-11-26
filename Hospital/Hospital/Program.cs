13f77e0e6159f8e2b03a38dc6ee135f67be904e8
Ahmed Hani <ahmedalmoselhy.slm@gmail.com>
2017-11-26 11:50:13 +0200

    Step1

---
+++ Hospital/Hospital/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    //The Hospital Details
+    class hospital
+    {
+        string name;
+        public hospital(string n)
+        {
+            this.name = n;
+        }
+    }
+    class department
+    {
+        string name;
+        int d_num;
+        int r_num;
+        int p_num;
+        public department(string n, int d, int r, int p)
+        {
+            this.name = n;
+            this.d_num = d;
+            this.r_num = r;
+            this.p_num = p;
+        }
+    }
+    class doctor
+    {
+        string name;
+        int age;
+        string spec;
+        public doctor(string n, string s, int a)
+        {
+            this.name = n;
+            this.spec = s;
+            this.age = a;
+        }
+    }
+}
